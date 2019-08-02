d969e7ac52a57b4f2d54d67fe3d405d9aeb79979
ValentinMolina500 <43248817+ValentinMolina500@users.noreply.github.com>
2019-08-01 20:05:58 -0700

    Updates

---
+++ precache-manifest.8b1f06bc6f0f0c7412a3e8d9c53d5dcf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8df6d32cc6a3702dab6357701f8c62a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "257e0ee60fb3b5708cf8",
+    "url": "/static/css/main.4bd7baa4.chunk.css"
+  },
+  {
+    "revision": "d406c3ecb328065709ef",
+    "url": "/static/js/2.69f231d3.chunk.js"
+  },
+  {
+    "revision": "257e0ee60fb3b5708cf8",
+    "url": "/static/js/main.5528325a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
